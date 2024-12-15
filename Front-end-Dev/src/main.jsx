f4226e06f166e962b72a9166152b29816405ab02
Nelson19so <nelsonsomto10@gmail.com>
2024-12-15 14:11:17 +0100

    Nelson19so

--- Front-end-Dev/src/main.jsx
+++ Front-end-Dev/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+/**
+ * Style linking
+ */
+import "./assets/css/font-awesome.min.css";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/main.css";
+
+/**
+ * Javascript linking
+ */
+import "./assets/js/jquery.slicknav.js";
+import "./assets/js/bootstrap.min.js";
+import "./assets/js/jquery.magnific-popup.min.js";
+import "./assets/js/jquery-3.3.1.min.js";
+
+/**
+ * Renders all element to the Html file, making it a single page site
+ */
+createRoot(document.getElementById("root-dowcument-src")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
