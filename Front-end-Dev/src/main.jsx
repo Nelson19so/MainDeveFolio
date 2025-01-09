daa7e02f397f1840a66dfde2c518e276d8e084e9
Nelson19so <nelsonsomto10@gmail.com>
2025-01-09 11:11:44 +0100

    Nelson19so

--- Front-end-Dev/src/main.jsx
+++ Front-end-Dev/src/main.jsx
@@ -12,7 +12,6 @@ import "./assets/css/main.css";
 /**
  * Javascript linking
  */
-// import "./assets/js/vendor/jquery.slicknav.js";
 import "./assets/js/vendor/jquery-3.3.1.min.js";
 import "./assets/js/vendor/jquery.magnific-popup.min.js";
 import "./assets/js/vendor/bootstrap.min.js";
