0a50779ac7550b966ae4777ca85eaf1275f15c73
Nelson19so <nelsonsomto10@gmail.com>
2025-01-22 16:14:32 +0100

    Nelson19so

--- Front-end-Dev/src/main.jsx
+++ Front-end-Dev/src/main.jsx
@@ -6,16 +6,15 @@ import App from "./App.jsx";
  * Style linking
  */
 import "./assets/css/vendor/font-awesome.min.css";
-import "./assets/css/vendor/bootstrap.min.css";
+// import "./assets/css/vendor/bootstrap.min.css";
 import "./assets/css/main.css";
 
 /**
  * Javascript linking
  */
-// import "./assets/js/vendor/jquery.slicknav.js";
 import "./assets/js/vendor/jquery-3.3.1.min.js";
 import "./assets/js/vendor/jquery.magnific-popup.min.js";
-import "./assets/js/vendor/bootstrap.min.js";
+// import "./assets/js/vendor/bootstrap.min.js";
 
 /**
  * Renders all element to the Html file, making it a single page site
