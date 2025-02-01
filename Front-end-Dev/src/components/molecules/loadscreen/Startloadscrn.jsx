00f6894be3b756f40cd0acbda90d2e25463c5d5f
Nelson19so <nelsonsomto10@gmail.com>
2025-02-01 13:48:14 +0100

    Nelson19so

--- Front-end-Dev/src/components/molecules/loadscreen/Startloadscrn.jsx
+++ Front-end-Dev/src/components/molecules/loadscreen/Startloadscrn.jsx
@@ -1,15 +1,15 @@
 export default function Startloadscrn() {
-  // $(window).on("load", function () {
-  //   $(".container-load-screen").delay(700).fadeOut(700);
-  //   $(".spinner-border").delay(800).fadeOut(600);
-  // });
+  $(window).on("load", function () {
+    $(".container-load-screen").delay(700).fadeOut(700);
+    $(".spinner-border").delay(800).fadeOut(600);
+  });
   return (
     <>
-      {/* <div className="container-load-screen">
+      <div className="container-load-screen">
         <center>
           <div className="spinner-border"></div>
         </center>
-      </div> */}
+      </div>
     </>
   );
 }
